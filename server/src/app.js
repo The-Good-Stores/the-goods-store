40a1dcaaa74a9ee651fa9ca78ee3c3e05d074005
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-11-05 11:22:20 -0400

    C2S5G1-10 project structure setup

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const api = require("./routes/api");
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use("/api", api);
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+module.exports = app;
