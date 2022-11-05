df04953ee6f21028aec8f4372cbb5e5b545e6af5
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-11-05 10:52:07 -0400

    C2S5G1-10 express setup

---
+++ server/server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
