246fb6bd79662cf5a5b8f523b844557a806cefa5
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-11-05 12:15:23 -0400

    nav bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
