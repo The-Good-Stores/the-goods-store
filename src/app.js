64199575e86e8291a28fa1fb4010945951626df8
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-11-07 19:46:05 -0500

    folder structure changed

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(
 app.use("/api", api);
 app.use(morgan("combined"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname));
